84c9f5a1038d586f240fed00ebc7ec32624c311c
Innocent <37780674+Kisheo@users.noreply.github.com>
2023-04-02 14:42:42 +0300

    Fetching today's News

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+//database
 $servername = "localhost";
 $username = "username";
 $password = "password";
@@ -11,5 +12,26 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+//fetching news
+$url = "http://api.mediastack.com/v1/news?access_key=f32591403d2b1dc28ff8769ad5f63a27&languages=en&countries=us&date=" . date("Y-m-d");
+
+// Initialize cURL
+$ch = curl_init();
+
+// Set the URL
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Return the response instead of outputting it
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the request
+$response = curl_exec($ch);
+
+// Close cURL
+curl_close($ch);
+
+// Decode the JSON response
+$news = json_decode($response, true);
 
 ?>
